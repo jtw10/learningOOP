a4f335924da8ae439f2368ae8ec1ab76638e6333
Josh W <jtwong10@gmail.com>
2019-08-14 20:13:48 -0700

    learning about subclasses

--- learning.py
+++ learning.py
@@ -65,3 +65,94 @@ print(bb_2.iq_growth)
 # 2019-08-13 is a tuesday, so the BigBrain.is_weekend() of this date should return false
 my_date = datetime.date(2019, 8, 13)
 print(BigBrain.is_weekend(my_date))
+
+
+# student is a subclass of BigBrain, and it inherits from this class
+class Student(BigBrain):
+
+    # by changing the growth rate of a subclass, it doesn't change anything in parent class
+    iq_growth = 1.05
+
+    # when we want to init subclass with more variables, we must give Students its own init method
+    def __init__(self, first, last, iq, gpa):
+
+        # the BigBrains init method will handle the first name, last name, and iq
+        super().__init__(first, last, gpa)
+
+        # the Student subclass will handle the gpa only during the init
+        self.gpa = gpa
+
+# Instructor is another subclass of BigBrain
+
+
+class Instructor(BigBrain):
+
+    # pass a list of students as an argument, but mutable variables should not be passed as an arg, so students=None
+    def __init__(self, first, last, iq, students=None):
+        super().__init__(first, last, iq)
+
+        # if there are no students, set it to an empty list. otherwise students = students
+        if students is None:
+            self.students = []
+        else:
+            self.students = students
+
+    # a method to add students
+    def add_stud(self, stud):
+        if stud not in self.students:
+            self.students.append(stud)
+
+    # a method to remove students
+    def rem_stud(self, stud):
+        if stud in self.students:
+            self.students.remove(stud)
+
+    # method to print out all students under this instructor
+    def print_student_list(self):
+        for stud in self.students:
+            print('-->', stud.fullname())
+
+
+student1 = Student('dhruv', 'ppap', 9001, 4.3)
+student2 = Student('jim', 'bobby', 9001, 4.3)
+
+instructor1 = Instructor('little', 'benjamin', 9001, [student1])
+
+print(student1.first)
+print(student2.first)
+print(instructor1.fullname())
+
+instructor1.add_stud(student2)
+instructor1.print_student_list()
+
+# help function shows method resolution order, where python searches for attributes and methos
+# print(help(Student))
+
+""" 
+isinstance() tells us if object is isntance of a class
+for example, the following would return true true false: 
+isinstance(instructor1, Instructor)
+isinstance(instructor1, BigBrain)
+isinstance(instructor1, Student)
+
+instructor1 is an instance of instructor
+instructor1 is an instance of bigbrain, since instructor is subclass of bigbrain
+instructor1 is not an instance of a student
+"""
+print(isinstance(instructor1, Instructor))
+print(isinstance(instructor1, BigBrain))
+print(isinstance(instructor1, Student))
+
+"""
+issubclass() tells us if something is an subclass of something else
+for example, the following would return true true false:
+issubclass(Instructor, BigBrain)
+issubclass(Student, BigBrain)
+issubclass(BigBrain, Instructor)
+
+Instructor and Student are subclass of BigBrain
+BigBrain is not a subclass of Instructor
+"""
+print(issubclass(Instructor, BigBrain))
+print(issubclass(Student, BigBrain))
+print(issubclass(BigBrain, Instructor))
